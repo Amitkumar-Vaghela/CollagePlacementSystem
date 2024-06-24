c0abea958926ae8467e858a0ede050c6d9ca65b1
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-06-24 22:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-from flask import Flask, request, render_template
-from flask_pymongo import PyMongo
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/flask_db"
-mongo = PyMongo(app)
+from flask import render_template
+from placement_management import app, db
+from placement_management.models import User, Placement
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        content = request.form['content']
-        degree = request.form['degree']
-        mongo.db.todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('index'))
-
-    todos = mongo.db.todos.find()
-    return render_template('index.html', todos=todos)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def home():
+    placements = Placement.query.all()
+    return render_template('index.html', placements=placements)
